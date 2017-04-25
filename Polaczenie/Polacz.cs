4495d29fff79715820469389a5debd98abe38816
Rafał <Rafa@GORSKI>
2017-04-25 19:27:29 +0200

    Dodaj pliki projektów.

---
+++ Polaczenie/Polacz.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tabele;
+
+namespace Polaczenie
+{
+    public class Polacz
+    {
+        SqlConnection conn;
+        SqlConnectionStringBuilder connStringBuilder;
+
+        void Polaczenie()
+        {
+            connStringBuilder = new SqlConnectionStringBuilder();
+            connStringBuilder.DataSource = "GORSKI";
+            connStringBuilder.InitialCatalog = "Warsztat";
+            connStringBuilder.IntegratedSecurity = true;
+
+            conn = new SqlConnection(connStringBuilder.ToString());
+        }
+
+        public Polacz()
+        {
+            Polaczenie();
+        }
+
+        public void Loguj (Uzytkownicy u )
+        {
+            try
+            {
+               
+                //string cmdText = "SELECT* FROM dbo.Uzytkownicy (Login,Haslo,Typ) VALUES (@Login) ";
+                //SqlCommand cmd = new SqlCommand(cmdText,conn);
+                //cmd.Parameters.AddWithValue("@Login",u.Login);
+                //cmd.Parameters.AddWithValue("@Haslo", u.Haslo);
+                //cmd.Parameters.AddWithValue("@Typ", u.Typ);
+                string cmdText = "SELECT * FROM dbo.Uzytkownicy WHERE Login = '" + u.Login + "' AND Haslo = '" + u.Haslo + "';";
+               
+                
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(cmdText, conn);
+                cmd.Parameters.AddWithValue("@Login",u.Login );
+                cmd.Parameters.AddWithValue("@Haslo",u.Haslo);
+                cmd.Parameters.AddWithValue("@Typ", u.Typ);
+               
+                cmd.ExecuteNonQuery();
+
+
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+
+            finally
+            {
+                if (conn !=null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Tabele/Uzytkownicy.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabele
+{
+    public class Uzytkownicy
+    {
+       
+          //  private int idUzytkownika=1;
+            private string login="Szefo";
+            private string haslo="";
+            private string typ="";
+
+            //public int IdUzytkownika
+            //{
+            //    get
+            //    {
+            //        return idUzytkownika;
+            //    }
+
+            //    set
+            //    {
+            //        idUzytkownika = value;
+            //    }
+            //}
+
+            public string Login
+            {
+                get
+                {
+                    return login;
+                }
+
+                set
+                {
+                    login = value;
+                }
+            }
+
+            public string Haslo
+            {
+                get
+                {
+                    return haslo;
+                }
+
+                set
+                {
+                    haslo = value;
+                }
+            }
+
+            public string Typ
+            {
+                get
+                {
+                    return typ;
+                }
+
+                set
+                {
+                    typ = value;
+                }
+            }
+        }
+    }
+
